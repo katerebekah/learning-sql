9fba9d2b359cbd1243674b5c35cf7322528fb643
Kate <katerebekah@gmail.com>
2015-09-18 09:30:32 -0500

    Update 24.sql

--- 24.sql
+++ 24.sql
@@ -1,5 +1,5 @@
 SELECT Track.Name, SUM(InvoiceLine.Quantity) AS 'Number of Purchases' FROM Track
 JOIN InvoiceLine ON Track.TrackId = InvoiceLine.TrackId
 JOIN Invoice ON InvoiceLine.InvoiceId = Invoice.InvoiceId
-WHERE Invoice.InvoiceDate BETWEEN '2013-1-1' and '2013-12-31'
+WHERE Invoice.InvoiceDate BETWEEN '2013-1-1' and '2014-1-1'
 GROUP BY Track.TrackId;
